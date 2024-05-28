e87145a6fbbc238c046c2901fb3f56ac35f549d6
Mariano <marianogotardo@gmail.com>
2024-05-28 13:46:52 -0300

    Punto 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Tarea;
 Console.WriteLine("Hello, World!");
+
+List<Tareas> tareasPendientes = new List<Tareas>();
+List<Tareas> tareasRealizadas = new List<Tareas>();
+
+Random random = new Random();
+
+int n = random.Next(1,20);
+
+for (int i = 0; i < n; i++)
+{
+    Tareas tarea = new Tareas(i, $"Tarea Pendiente {i+1}",20,estadoTarea.Pendiente);
+    tareasPendientes.Add(tarea);
+}
+
+foreach (Tareas mostrarTarea in tareasPendientes)
+{
+    Console.WriteLine(mostrarTarea.Descripcion);
+}
+
+moverTareas(tareasPendientes,tareasRealizadas);
+
+
+static void moverTareas(List<Tareas> tareasPendientes,List<Tareas> tareasRealizadas)
+{
+    foreach (Tareas buscar in tareasPendientes)
+    {
+        tareasRealizadas.Add(buscar);
+        tareasPendientes.Remove(buscar);
+    }
+}
+
+
+
+
+
+
---
+++ Tarea.cs
@@ -0,0 +1,65 @@
+namespace Tarea;
+
+public class Tareas
+{
+    private int tareaID;
+
+    private string descripcion;
+
+    private int duracion;
+
+    private estadoTarea estado;
+
+    public int TareaID
+    {
+        get
+        {
+            return tareaID;
+        }
+        set
+        {
+            tareaID = value;
+        }
+    }
+
+    public string Descripcion
+    {
+        get
+        {
+            return descripcion;
+        }
+        set
+        {
+            descripcion = value;
+        }
+    }
+
+    public int Duracion
+    {
+        get
+        {
+            return duracion;
+        }
+        set
+        {
+            duracion = value;
+        }
+    }
+
+    public estadoTarea Estado { get => estado; set => estado = value; }
+
+    public Tareas(int tareaID, string descripcion, int duracion, estadoTarea estadoDeTarea)
+    {
+        this.tareaID = tareaID;
+        this.descripcion = descripcion;
+        this.duracion = duracion - 1;
+        Estado = estadoDeTarea;
+    }
+
+}
+
+public enum estadoTarea
+{
+    Pendiente,
+    Realizada,
+}
